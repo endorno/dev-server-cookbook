91a3a52544636090dd0bd2bc3560a2c6d4940b73
Teppei Fujisawa <teppei.fujisawa@dena.jp>
2013-05-20 14:24:45 +0900

    Added zsh,vim setting

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,39 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'zsh' do
+  action :install
+end
+
+package 'vim' do
+  action :install
+end
+
+user_name = node["user"]["develop"]
+home_directory = "/home/"+user_name
+
+git home_directory+'/dotfiles' do
+  user user_name
+  group user_name
+  repository  "git://github.com/endorno/dotfiles.git"
+  reference "master"
+  action :sync
+#  enable_submodules true
+end
+
+dotfiles = [".vimrc", ".zshrc", ".vim" ]
+dotfiles.each do |f|
+  link home_directory+"/"+f do
+    to home_directory+"/dotfiles/"+f
+    link_type :symbolic
+  end
+end
+
+bash "change default shell" do
+  user user_name
+  code <<-EOL
+  chsh -s `which zsh`
+  EOL
+end
+
